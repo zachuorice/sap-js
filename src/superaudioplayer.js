9e9c27178a330594d02395eb2e580c7313c86832
Zachary Richey <zr.public@gmail.com>
2015-08-19 13:43:10 -0400

    src/superaudioplayer.js: Setup empty saplayer module and jquery function.

--- src/superaudioplayer.js
+++ src/superaudioplayer.js
@@ -21,5 +21,14 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-// Nothing here, yet.
-var sap = {};
+var saplayer = (function() {
+	return {
+	};
+})();
+
+(function($) {
+	$.fn.saplayer = function() {
+		// TODO
+		return this;
+	}
+})(jQuery);
